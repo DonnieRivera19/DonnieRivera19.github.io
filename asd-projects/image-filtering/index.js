99efa79e7991465d8e1cc1bf0d1267ad241403d8
DonnieRivera19 <157040922+DonnieRivera19@users.noreply.github.com>
2025-03-28 13:12:32 +0000

    submit image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
   
 
   // do not change the below line of code
@@ -32,18 +34,57 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter(filterFunction) {
+  for(var i = 0; i < image.length; i++) {
+    for(var x = 0; x < image[i].length; x++) {
+      var rgbString = image[i][x];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers); 
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][x] = rgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+  var backgroundColor = image[0][0];
+  for (var w = 0; w < image.length; w++) {
+     var row = image[w];
+    for (var q = 0; q < row.length; q++) {
+        var rgbString = row[q];
+        if (rgbString !== backgroundColor) {
+          var rgbNumbers = rgbStringToArray(rgbString);
+          filterFunction(rgbNumbers);
+          rgbString = rgbArrayToString(rgbNumbers);
+          row[q] = rgbString;
+        }
+      }
+    }
+  }
 
 
 // TODO 5: Create the keepInBounds function
+function keepInBounds(num) {
+  
+ return Math.min(255, Math.max(0, num));
 
+}
 
 // TODO 3: Create reddify function
-
+function reddify(hey) {
+  hey[RED] = 200;
+  //hey[0] = 200;
+}
 
 // TODO 6: Create more filter functions
+function decreaseBlue(madi) {
+  //madi[BLUE] -= 50;
+  //keepInBounds(madi[BLUE]);
+  madi[BLUE] = keepInBounds(madi[BLUE] - 50);
+}
 
-
+function increaseGreenByBlue(donnie) {
+  donnie[GREEN] = keepInBounds(donnie[GREEN] + donnie[BLUE]);
+}
 // CHALLENGE code goes below here
